dfc315b8640934ef02beabdc1562c5d7800f83f9
vladisluw <komkov222111@gmail.com>
2023-08-03 22:09:29 +0500

    Верстка секции Экс. Предложение с анимацией и адаптивом

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,10 @@ import switchBanner from './modules/switchBanner.js'
 import switchTools from './modules/switchTools.js'
 import arrows from './modules/arrows.js'
 import switchFeatures from './modules/switchFeatures.js'
+import switchPromo from './modules/switchPromo.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   swipingBlock(document.querySelector('.banners__images'))
   swipingBlock(document.querySelector('.features__items'))
+  swipingBlock(document.querySelector('.promo__items'))
 })
---
+++ src/php/__promo.php
@@ -0,0 +1,70 @@
+<div class="promo">
+  <div class="promo__container ">
+    <h2 class="promo__header container">
+      ЭКСКЛЮЗИВНОЕ ПРЕДЛОЖЕНИЕ<br><span>дом по стоимости однокомнатной квартиры в Челябинске</span>
+    </h2>
+    <div class="promo__items">
+      <acticle class="promo__item promo-item promo-item-1">
+        <img src="@img/promo/promo-1.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-2">
+        <img src="@img/promo/promo-2.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-3">
+        <img src="@img/promo/promo-3.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-4">
+        <img src="@img/promo/promo-4.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-5">
+        <img src="@img/promo/promo-5.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-6">
+        <img src="@img/promo/promo-6.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-1">
+        <img src="@img/promo/promo-1.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-2">
+        <img src="@img/promo/promo-2.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-3">
+        <img src="@img/promo/promo-3.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-4">
+        <img src="@img/promo/promo-4.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-5">
+        <img src="@img/promo/promo-5.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-6">
+        <img src="@img/promo/promo-6.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-1">
+        <img src="@img/promo/promo-1.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-2">
+        <img src="@img/promo/promo-2.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-3">
+        <img src="@img/promo/promo-3.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-4">
+        <img src="@img/promo/promo-4.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-5">
+        <img src="@img/promo/promo-5.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+      <acticle class="promo__item promo-item promo-item-6">
+        <img src="@img/promo/promo-6.jpg" alt="Дом по стоимости однокомнатной квартиры в Челябинске">
+      </acticle>
+    </div>
+    <div class="promo__btn-container container">
+      <button class="promo__btn btn">
+        <p>УЗНАТЬ ПОДРОБНЕЕ</p>
+        <div class="btn-hover"></div>
+      </button>
+    </div>
+
+  </div>
+</div>
